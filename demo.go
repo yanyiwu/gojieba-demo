6e7e937106ce1673531ceb0b151ac366633d989e
yanyiwu <wuyanyi09@foxmail.com>
2024-08-28 23:20:05 +0800

    add demo and using gojieba v1.4.2

--- demo.go
+++ demo.go
@@ -7,9 +7,7 @@ import (
 	"github.com/yanyiwu/gojieba"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-
+func demo1() {
 	var s string
 	var words []string
 	use_hmm := true
@@ -44,4 +42,26 @@ func main() {
 	words = x.Extract(s, 3)
 	fmt.Println(s)
 	fmt.Println("关键词抽取:", strings.Join(words, ","))
+}
+
+func demo2() {
+	var s string
+	var words []string
+	use_hmm := true
+	x := gojieba.NewJieba(
+		"",
+		"",
+		"./userdict.utf8",
+		"",
+		"")
+	defer x.Free()
+	s = "红掌拨清波"
+	words = x.Cut(s, use_hmm)
+	fmt.Println(s)
+	fmt.Println("精确模式:", strings.Join(words, "/"))
+}
+
+func main() {
+	demo1()
+	demo2()
 }
